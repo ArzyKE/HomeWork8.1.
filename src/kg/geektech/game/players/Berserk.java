044695606db23310dcb4ca982af2b21983df5a6a
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2022-01-15 20:05:40 +0600

    Initial commit

---
+++ src/kg/geektech/game/players/Berserk.java
@@ -0,0 +1,12 @@
+package kg.geektech.game.players;
+
+public class Berserk extends Hero {
+    public Berserk(int health, int damage, String name) {
+        super(health, damage, SuperAbility.SAVE_DAMAGE_AND_REVERT, name);
+    }
+
+    @Override
+    public void applySuperPower(Hero[] heroes, Boss boss) {
+
+    }
+}
